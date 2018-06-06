42d57c06dc993373ab3eea6965b05183f50ad999
Fabio Melloni <fabio.melloni@futuresolutionservices.com>
2018-06-06 14:13:20 +0200

    Add WebDAV container

---
+++ webdav/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+chmod a+rwx /media -R
+if [[ -n "$USERNAME" ]] && [[ -n "$PASSWORD" ]]
+then
+        htpasswd -bc /etc/nginx/htpasswd $USERNAME $PASSWORD
+        echo Done.
+else
+    echo Using no auth.
+        sed -i 's%auth_basic "Restricted";% %g' /etc/nginx/conf.d/default.conf
+        sed -i 's%auth_basic_user_file htpasswd;% %g' /etc/nginx/conf.d/default.conf
+fi
